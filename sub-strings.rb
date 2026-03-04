38ff0944eb3c7da4b3cd0ebb2aca7d18c5026ad1
jameshsims91 <jameshsims91@gmail.com>
2026-03-04 11:13:02 -0500

    Finish code for the substrings project
    Finished adding the remaining code for substrings project.
    03/04/2026
    -JSims

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,9 +1,9 @@
+dictionary = %w(below down go going horn how howdy it i low own part partner sit)
+
 def substrings(word, dictionary)
   counts = Hash.new(0)
 
   word_downcase = word.downcase
-
-  debugger 
 
   dictionary.each do |substring|
     matches = word_downcase.scan(/#{substring}/i)
@@ -13,3 +13,5 @@ def substrings(word, dictionary)
   end
   counts
 end
+
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
